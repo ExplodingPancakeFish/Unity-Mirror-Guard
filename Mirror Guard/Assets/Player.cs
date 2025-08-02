1aa1aa89a90da8d04d64302799e30f42617b1546
RPenn18 <ryanapenn@outlook.com>
2025-08-02 12:36:45 -0700

    Basic Wall Climbing
    Still Needs to move smoother

--- Mirror Guard/Assets/Player.cs
+++ Mirror Guard/Assets/Player.cs
@@ -10,6 +10,8 @@ public class PlayerScript : MonoBehaviour
     public float DashSpd = 10;
     public float Dashcooldown;
     public float LastDir;
+    public bool LockMovement;
+    public float WallClimbTimer;
     //Jumping
     public float JumpForce;
     public float MaxJumpTime = 0.3f;
@@ -49,39 +51,84 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         // Set Horizontal Movement
-        if (Input.GetKey(KeyCode.A) == true)
+        if (LockMovement == false)
         {
-            MoveDir = -1;
-            LastDir = -1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            MoveDir = 1;
-            LastDir = 1;
-        }
-        else
-        {
-            MoveDir = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            if (Dashcooldown == 0)
+            if (Input.GetKey(KeyCode.A) == true)
             {
-                DashDuration = 10;
+                MoveDir = -1;
+                LastDir = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                MoveDir = 1;
+                LastDir = 1;
+            }
+            else
+            {
+                MoveDir = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if (Dashcooldown == 0)
+                {
+                    DashDuration = 10;
+                }
             }
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (TouchingRightWall == false && TouchingLeftWall == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 JumpBufferTimer = JumpBufferTime;
             }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                IsJumping = false;
+            }
+        }
+        else
         {
-            IsJumping = false;
+            if (LockMovement == false)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    LockMovement = true;
+                    WallClimbTimer = 10;
+                }
+            }
+
         }
     }
     void FixedUpdate()
     {
+        //Wall Jumping
+        if (WallClimbTimer > 0)
+        {
+                if (WallClimbTimer > 5)
+                {
+                    Debug.Log("Diagonal");
+                    if (TouchingLeftWall)
+                    {
+                        PlayerRB.linearVelocity = new Vector2(5, 5);
+                    }
+                    else
+                    {
+                        PlayerRB.linearVelocity = new Vector2(-5, 5);
+                    }
+                    Debug.Log(PlayerRB.linearVelocity);
+                }
+                else
+                {
+                    Debug.Log("Up");
+                    PlayerRB.linearVelocity = new Vector2(0, 5);
+                }
+                WallClimbTimer -= 1;
+                if (WallClimbTimer == 0)
+                {
+                    LockMovement = false;
+                }
+        }
         //Are you touching ground?
         IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, GroundLayer);
         if (IsGrounded)
@@ -105,30 +152,30 @@ public class PlayerScript : MonoBehaviour
         TouchingLeftWall = Physics2D.OverlapCircle(LeftWallCheck.position, 0.1f, WallLayer);
         if (TouchingLeftWall == false)
         {
-            TouchingLeftWall = Physics2D.OverlapCircle(LeftWallCheck.position, 0.1f, GroundWallLayer);            
+            TouchingLeftWall = Physics2D.OverlapCircle(LeftWallCheck.position, 0.1f, GroundWallLayer);
         }
         if (TouchingLeftWall)
-            {
+        {
 
-            }
-            else
-            {
+        }
+        else
+        {
 
-            }
+        }
         //are you touching a right wall
-            TouchingRightWall = Physics2D.OverlapCircle(RightWallCheck.position, 0.1f, WallLayer);
+        TouchingRightWall = Physics2D.OverlapCircle(RightWallCheck.position, 0.1f, WallLayer);
         if (TouchingRightWall == false)
         {
-            TouchingRightWall = Physics2D.OverlapCircle(RightWallCheck.position, 0.1f, GroundWallLayer);               
+            TouchingRightWall = Physics2D.OverlapCircle(RightWallCheck.position, 0.1f, GroundWallLayer);
         }
         if (TouchingRightWall)
-            {
+        {
 
-            }
-            else
-            {
+        }
+        else
+        {
 
-            }
+        }
         if (TouchingLeftWall || TouchingRightWall)
         {
             WallSlowing = new Vector2(0, 10);
@@ -141,13 +188,16 @@ public class PlayerScript : MonoBehaviour
         }
 
         // Apply Horizontal Movement
-            PlayerRB.linearVelocity = new Vector2(MoveDir * MoveSpd, PlayerRB.linearVelocity.y);
-        // Apply Vertical Movement
-        if (IsJumping && JumpTimeCounter > 0 && Input.GetKey(KeyCode.Space))
+        if (LockMovement == false)
         {
-            PlayerRB.linearVelocity += Vector2.up * JumpHoldForce * Time.fixedDeltaTime;
-            JumpTimeCounter -= Time.fixedDeltaTime;
+            PlayerRB.linearVelocity = new Vector2(MoveDir * MoveSpd, PlayerRB.linearVelocity.y);
         }
+        // Apply Vertical Movement
+            if (IsJumping && JumpTimeCounter > 0 && Input.GetKey(KeyCode.Space))
+            {
+                PlayerRB.linearVelocity += Vector2.up * JumpHoldForce * Time.fixedDeltaTime;
+                JumpTimeCounter -= Time.fixedDeltaTime;
+            }
         if (JumpBufferTimer > 0 && CoyoteTimer > 0)
         {
             PlayerRB.linearVelocity = new Vector2(PlayerRB.linearVelocity.x, JumpForce);
@@ -172,17 +222,17 @@ public class PlayerScript : MonoBehaviour
             Dashcooldown -= 1;
         }
         //Falling Acceleration
-            if (PlayerRB.linearVelocity.y < 0)
-            {
-                FallTime += Time.fixedDeltaTime; // increase fall duration
-                FallTime = Mathf.Min(FallTime, 1.5f);//terminalvelocity
-                fallVelocity = Vector2.down * FallAcceleration * FallTime;
-                fallVelocity += WallSlowing;
-                PlayerRB.linearVelocity += fallVelocity * Time.fixedDeltaTime;
-            }
-            else
-            {
-                FallTime = 0; // reset when not falling
-            }
+        if (PlayerRB.linearVelocity.y < 0)
+        {
+            FallTime += Time.fixedDeltaTime; // increase fall duration
+            FallTime = Mathf.Min(FallTime, 1.5f);//terminalvelocity
+            fallVelocity = Vector2.down * FallAcceleration * FallTime;
+            fallVelocity += WallSlowing;
+            PlayerRB.linearVelocity += fallVelocity * Time.fixedDeltaTime;
+        }
+        else
+        {
+            FallTime = 0; // reset when not falling
+        }
     }
 }
